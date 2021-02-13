a1927345d83ff7c4bd1f40007b9d8bf0aef5e029
Bowers-L <loganmbowers@gmail.com>
2021-02-13 15:27:03 -0500

    Input Controls

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerInputController : MonoBehaviour
+{
+    public float X
+    {
+        get;
+        private set;
+    }
+
+    public float Y
+    {
+        get;
+        private set;
+    }
+
+
+
+    public bool Light {
+        get;
+        private set;
+    }
+
+    public bool Heavy
+    {
+        get;
+        private set;
+    }
+
+    public bool Interact
+    {
+        get;
+        private set;
+    }
+
+    public bool Block
+    {
+        get;
+        private set;
+    }
+
+    public bool Roll
+    {
+        get;
+        private set;
+    }
+
+    private void Update()
+    {
+        Input.GetAxis("")
+    }
+}

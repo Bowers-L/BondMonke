37bafcf08689ca897fb6c1a443f077f94d7f92ea
TheGrim159 <stdrew190@gmail.com>
2021-04-15 18:55:53 -0400

    Final banana and swing axe

---
+++ Assets/OurAssets/Scripts/Interactables/FinalCollectableUi.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalCollectableUi : MonoBehaviour
+{
+    private GameObject player;
+    private CanvasGroup canvasGroup;
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            Debug.LogError("The component CanvasGroup is missing");
+        }
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+        {
+            Debug.LogError("Player is not found");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<BallCollector>().final)
+        {
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.alpha = 1f;
+            player.GetComponent<PlayerInputController>().enabled = false;
+            Time.timeScale = 0f;
+        }
+    }
+}

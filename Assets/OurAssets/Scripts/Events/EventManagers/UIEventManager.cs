9ac2ec9fb88d6342d90ad87ef8e7e21d5d3bbcc7
Loiosh <30377390+Spellbreeze@users.noreply.github.com>
2021-04-29 09:18:30 -0400

    Credits & Control Menu
    Also stopped tutorial prompts and pause menu from messing with each other

--- Assets/OurAssets/Scripts/Events/EventManagers/UIEventManager.cs
+++ Assets/OurAssets/Scripts/Events/EventManagers/UIEventManager.cs
@@ -6,20 +6,22 @@ using UnityEngine.UI;
 
 public class UIEventManager : MonoBehaviour
 {
-
+    private UnityAction respawnListener;
     private UnityAction<bool> fadeListener;
     public GameObject blackPanel;
     public float fadeSpeed = 1;
+    public float delay = 0.5f;
 
     void Awake()
     {
         fadeListener = new UnityAction<bool>(fadeHandler);
+        respawnListener = new UnityAction(respawnHandler);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -31,18 +33,48 @@ public class UIEventManager : MonoBehaviour
     private void OnEnable()
     {
         EventManager.StartListening<FadeEvent, bool>(fadeListener);
+        EventManager.StartListening<RespawnEvent>(respawnListener);
     }
     private void OnDisable()
     {
         EventManager.StopListening<FadeEvent, bool>(fadeListener);
+        EventManager.StopListening<RespawnEvent>(respawnListener);
     }
+
+    void respawnHandler()
+    {
+        Debug.Log("Handled fade");
+        StartCoroutine(Respawn());
+    }
+    public IEnumerator Respawn()
+    {
+        Color panelColor = blackPanel.GetComponent<Image>().color;
+        float fadeAmount;
+
+        while (blackPanel.GetComponent<Image>().color.a < 1)
+        {
+            fadeAmount = panelColor.a + (fadeSpeed * Time.deltaTime);
+            panelColor = new Color(panelColor.r, panelColor.g, panelColor.b, fadeAmount);
+            blackPanel.GetComponent<Image>().color = panelColor;
+            yield return null;
+        }
+        yield return new WaitForSeconds(delay);
+        while (blackPanel.GetComponent<Image>().color.a > 0)
+        {
+            fadeAmount = panelColor.a - (fadeSpeed * Time.deltaTime);
+            panelColor = new Color(panelColor.r, panelColor.g, panelColor.b, fadeAmount);
+            blackPanel.GetComponent<Image>().color = panelColor;
+            yield return null;
+        }
+    }
+
 
     void fadeHandler(bool fadeDir)
     {
-        Debug.Log("Handled fade");
-        StartCoroutine(FadeOut(fadeDir));
+        Debug.Log("Handled fade in/out");
+        StartCoroutine(FadeIO(fadeDir));
     }
-    public IEnumerator FadeOut(bool fadeOut = true)
+    public IEnumerator FadeIO(bool fadeOut = true)
     {
         Color panelColor = blackPanel.GetComponent<Image>().color;
         float fadeAmount;
@@ -56,7 +88,8 @@ public class UIEventManager : MonoBehaviour
                 blackPanel.GetComponent<Image>().color = panelColor;
                 yield return null;
             }
-            yield return new WaitForSeconds(1);
+        }
+        else {
             while (blackPanel.GetComponent<Image>().color.a > 0)
             {
                 fadeAmount = panelColor.a - (fadeSpeed * Time.deltaTime);
